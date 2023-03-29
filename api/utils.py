aab021e23c8655b567c8799e255debf7ffe43e00
quan <minhquan.vo01@gmail.com>
2023-03-29 21:59:53 +0700

    .

--- api/utils.py
+++ api/utils.py
@@ -37,7 +37,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TRAINNED_MODEL = "temp"
-# TRAINNED_MODEL = keras.models.load_model(os.environ.get('TRAINNED_MODEL_PATH'))
+TRAINNED_MODEL = keras.models.load_model(os.environ.get('TRAINNED_MODEL_PATH'))
 THREAD_QUANTITY_CRAWL_PRODUCT = int(
     os.environ.get('THREAD_QUANTITY_CRAWL_PRODUCT'))
 THREAD_NUMBER_LINK_SOURCE = int(
